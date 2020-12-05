b788d47ecf67c7930a8facc98ae21006f8820038
ivelin-web <72993041+ivelin-web@users.noreply.github.com>
2020-12-05 21:43:05 +0200

    Updated script
    Progress bar animation updated. Min screen width required for animation - 768px

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 /// <reference path='../typings/globals/jquery/index.d.ts' />
 
-$(document).ready(function() {
-
+$(document).ready(function () {
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
-    }
+    };
 
     animateNav();
     userAnimation();
@@ -15,132 +14,134 @@ $(document).ready(function() {
 
     // Animated Navbar
 
-    $('.first-button').on('click', function () {
-
-        $('.animated-icon1').toggleClass('open');
-      });
-      $('.second-button').on('click', function () {
-    
-        $('.animated-icon2').toggleClass('open');
-      });
-      $('.third-button').on('click', function () {
-    
-        $('.animated-icon3').toggleClass('open');
+    $(".first-button").on("click", function () {
+        $(".animated-icon1").toggleClass("open");
+    });
+    $(".second-button").on("click", function () {
+        $(".animated-icon2").toggleClass("open");
+    });
+    $(".third-button").on("click", function () {
+        $(".animated-icon3").toggleClass("open");
     });
 
     // Animated Progress Bar
     $(window).on("scroll", progressBarAnimation);
-    $(window).bind('resize', progressBarAnimation);
+    $(window).bind("resize", progressBarAnimation);
 
-    // Header Animations 
+    // Header Animations
     $(window).on("scroll", animateNav);
-    $(window).bind('resize', animateNav);
+    $(window).bind("resize", animateNav);
 
     // About Animations
     $(window).on("scroll", userAnimation);
-    $(window).bind('resize', userAnimation);
+    $(window).bind("resize", userAnimation);
 
-    // Experience Animations 
+    // Experience Animations
     $(window).on("scroll", experienceAnimation);
-    $(window).bind('resize', experienceAnimation);
+    $(window).bind("resize", experienceAnimation);
 
     // Interestings Animations
     $(window).on("scroll", interestingsAnimation);
-    $(window).bind('resize', interestingsAnimation);
+    $(window).bind("resize", interestingsAnimation);
 
     // Contacts Animations
     $(window).on("scroll", contactsAnimation);
-    $(window).bind('resize', contactsAnimation);
+    $(window).bind("resize", contactsAnimation);
 
     // Functions
     function animateNav() {
-      let currentScroll = $(this).scrollTop();
+        let currentScroll = $(this).scrollTop();
 
-      if ($(window).width() > 750 && currentScroll >= 100) {
-        $(".site-header nav").addClass("navbar-animated");
-      }
-      else {
-        $(".site-header nav").removeClass("navbar-animated");
-      }
+        if ($(window).width() > 750 && currentScroll >= 100) {
+            $(".site-header nav").addClass("navbar-animated");
+        } else {
+            $(".site-header nav").removeClass("navbar-animated");
+        }
     }
 
     function userAnimation() {
-      let currentScroll = $(this).scrollTop();
-      let windowHeight = $(window).height();
+        let currentScroll = $(this).scrollTop();
+        let windowHeight = $(window).height();
 
-      if (currentScroll >= ($("#about .user-info").offset().top - windowHeight) + ($("#about .user-info").height() / 2)) {
-        $(".user-info").addClass("user-animation-right");
-      }
-      
-      if (currentScroll >= ($("#about .user-more").offset().top - windowHeight) + ($("#about .user-more").height() / 2)) {
-        $(".user-more").addClass("user-animation-bottom");
-      }
+        if (currentScroll >= $("#about .user-info").offset().top - windowHeight + $("#about .user-info").height() / 2) {
+            $(".user-info").addClass("user-animation-right");
+        }
+
+        if (currentScroll >= $("#about .user-more").offset().top - windowHeight + $("#about .user-more").height() / 2) {
+            $(".user-more").addClass("user-animation-bottom");
+        }
     }
 
     function experienceAnimation() {
-      let currentScroll = $(this).scrollTop();
-      let windowHeight = $(window).height();
-      let cards = $(".card").toArray();
-      
-      for (let i = 0; i < cards.length; i++) {
-        if (currentScroll >= $(cards[i]).offset().top + ($(cards[i]).height() / 3) - windowHeight) {
-          if ($(window).width() > 768) {
-            setTimeout(function() {
-              $(cards[i]).addClass("item-pop");
-            }, i * 1000 / 5);
-          }
-          else {
-            $(cards[i]).addClass("item-pop");
-          }
+        let currentScroll = $(this).scrollTop();
+        let windowHeight = $(window).height();
+        let cards = $(".card").toArray();
+
+        for (let i = 0; i < cards.length; i++) {
+            if (currentScroll >= $(cards[i]).offset().top + $(cards[i]).height() / 3 - windowHeight) {
+                if ($(window).width() > 768) {
+                    setTimeout(function () {
+                        $(cards[i]).addClass("item-pop");
+                    }, (i * 1000) / 5);
+                } else {
+                    $(cards[i]).addClass("item-pop");
+                }
+            }
         }
-      }
     }
 
     function progressBarAnimation() {
-      let currentScroll = $(this).scrollTop();
-      let windowHeight = $(window).height();
-      let skills = $(".skill");
-
-      for (let i = 0; i < skills.length; i++) {
-        if (currentScroll >= ($(skills[i]).offset().top - windowHeight) 
-        && currentScroll <= ($(skills[i]).offset().top + ($(skills[i]).height() + 200))) {
-            let currentBar = $(".progress-bar")[i];
-            let procent = $(currentBar).attr("aria-valuenow");
-            $(currentBar).animate({
-                width: `${procent}%`
-            }, 2500);
+        // Work only for min width: 768px -> mobile phone not working!
+        if ($(window).width() < 768) {
+            return;
         }
-      }
+
+        let currentScroll = $(this).scrollTop();
+        let windowHeight = $(window).height();
+        let skills = $(".skill");
+
+        for (let i = 0; i < skills.length; i++) {
+            if (currentScroll >= $(skills[i]).offset().top - windowHeight && currentScroll <= $(skills[i]).offset().top + ($(skills[i]).height() + 200)) {
+                let currentBar = $(".progress-bar")[i];
+                let procent = $(currentBar).attr("aria-valuenow");
+                $(currentBar).animate(
+                    {
+                        width: `${procent}%`,
+                    },
+                    2500
+                );
+            }
+        }
     }
 
     function interestingsAnimation() {
-      let currentScroll = $(this).scrollTop();
-      let windowHeight = $(window).height();
-      let interest = $("#interest");
-      let items = $("#interest .item");
+        let currentScroll = $(this).scrollTop();
+        let windowHeight = $(window).height();
+        let interest = $("#interest");
+        let items = $("#interest .item");
 
-      for (let i = 0; i < items.length - 1; i += 2) {
-        // Left
-        if (currentScroll >= $(items[i]).offset().top + ($(items[i]).height() / 3) - windowHeight) {
-          $(items[i]).addClass("item-left");
-        }
+        for (let i = 0; i < items.length - 1; i += 2) {
+            // Left
+            if (currentScroll >= $(items[i]).offset().top + $(items[i]).height() / 3 - windowHeight) {
+                $(items[i]).addClass("item-left");
+            }
 
-        // Right
-        if (currentScroll >= $(items[i + 1]).offset().top + ($(items[i + 1]).height() / 3) - windowHeight) {
-          $(items[i + 1]).addClass("item-right");
+            // Right
+            if (currentScroll >= $(items[i + 1]).offset().top + $(items[i + 1]).height() / 3 - windowHeight) {
+                $(items[i + 1]).addClass("item-right");
+            }
         }
-      }
     }
 
     function contactsAnimation() {
         let currentScroll = $(this).scrollTop();
         let windowHeight = $(window).height();
 
-        if (currentScroll >= ($("#contactMe").offset().top - windowHeight) + ($("#contactMe").height() / 2)) {
+        if (currentScroll >= $("#contactMe").offset().top - windowHeight + $("#contactMe").height() / 2) {
             $("#contactMe").addClass("contacts-item-animation");
         }
 
-        if (currentScroll >= ($("#findMe").offset().top - windowHeight) + ($("#findMe").height() / 2)) {
+        if (currentScroll >= $("#findMe").offset().top - windowHeight + $("#findMe").height() / 2) {
             $("#findMe").addClass("contacts-item-animation");
         }
     }
